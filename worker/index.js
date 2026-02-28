f65813f2e71232b033b860c60873a7f191db1bfa
New-Weilai <future20152024@outlook.com>
2026-02-28 18:09:54 +0800

    fixing comment,pls wait

--- worker/index.js
+++ worker/index.js
@@ -24,7 +24,7 @@ router.options('*', () => {
             'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
             'Access-Control-Allow-Headers': 'Content-Type, Authorization',
             'Access-Control-Allow-Credentials': 'true',
-       */ }
+        }*/
     });
 });
 
